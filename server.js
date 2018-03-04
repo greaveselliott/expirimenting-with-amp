d027701dcaee55cdb301f0a83695d6647be2a163
Elliott Greaves <greaveselliott@me.com>
2018-03-04 00:48:20 +0000

    simple image component, decided to scrap this expirement to create a more detialed amp project

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ import path from 'path';
 const PORT = 8080;
 let app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/page-1', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/page-1.html'));
 });
 
 app.listen(PORT, () => {
